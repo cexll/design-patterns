f79e36a6fc6cbcda2892fdadf3a015bc279c4876
cexll <evanxian9@gmail.com>
2022-11-10 11:25:06 +0800

    feat: factory method

---
+++ factory-method/factory_test.go
@@ -0,0 +1,21 @@
+package factory_method
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFactory(m *testing.T) {
+	ak47, _ := getGun("ak47")
+	musket, _ := getGun("musket")
+
+	printDetails(ak47)
+	printDetails(musket)
+}
+
+func printDetails(g IGun) {
+	fmt.Printf("Gun: %s", g.getName())
+	fmt.Println()
+	fmt.Printf("Power: %d", g.getPower())
+	fmt.Println()
+}
