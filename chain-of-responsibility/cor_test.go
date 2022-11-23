c12c217ede28bfc5928a322070720f35842a3ffe
cexll <evanxian9@gmail.com>
2022-11-23 16:08:30 +0800

    feat: chain_of_responsibility

---
+++ chain-of-responsibility/cor_test.go
@@ -0,0 +1,24 @@
+package chain_of_responsibility
+
+import "testing"
+
+func TestCor(t *testing.T) {
+	cashier := &Cashier{}
+
+	// Set next for medical department
+	medical := &Medical{}
+	medical.setNext(cashier)
+
+	// Set next for doctor department
+	doctor := &Doctor{}
+	doctor.setNext(medical)
+
+	// Set next for reception department
+	reception := &Reception{}
+	reception.setNext(doctor)
+
+	patient := &Patient{name: "abc"}
+
+	// Patient visiting
+	reception.execute(patient)
+}
