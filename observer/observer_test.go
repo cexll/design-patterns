bc70fafcbaeb69f2226289415246bac5bf1271c7
cexll <evanxian9@gmail.com>
2022-11-28 15:43:17 +0800

    feat: observer

---
+++ observer/observer_test.go
@@ -0,0 +1,15 @@
+package observer
+
+import "testing"
+
+func TestObserver(t *testing.T) {
+	shirtItem := newItem("Nick Shirt")
+
+	observerFirst := &Customer{id: "abc@gmail.com"}
+	observerSecond := &Customer{id: "xyz@gmail.com"}
+
+	shirtItem.register(observerFirst)
+	shirtItem.register(observerSecond)
+
+	shirtItem.updateAvailability()
+}
